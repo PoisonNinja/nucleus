837db34d94a1d1f550ca1a4ce84870f01ce4b7c1
Jason Lu <jason@jasonxlu.com>
2022-07-02 23:04:45 -0500

    Get booting working!

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,10 @@
 #![no_main] // disable all Rust-level entry points
 
 use core::panic::PanicInfo;
+use limine::LimineMmapRequest;
+
+#[used]
+static MMAP_REQUEST: LimineMmapRequest = LimineMmapRequest::new(0);
 
 #[no_mangle] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
