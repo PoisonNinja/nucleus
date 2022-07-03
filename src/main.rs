af54e07dde089d255315c762f0d5ea70bf891823
Jason Lu <jason@jasonxlu.com>
2022-07-03 12:11:53 -0500

    Build binary instead of library

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,13 @@
 use core::panic::PanicInfo;
 use limine::LimineMmapRequest;
 
-#[used]
 static MMAP_REQUEST: LimineMmapRequest = LimineMmapRequest::new(0);
 
 #[no_mangle] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
     // this function is the entry point, since the linker looks for a function
     // named `_start` by default
+    MMAP_REQUEST.get_response().get().unwrap();
     loop {}
 }
 
