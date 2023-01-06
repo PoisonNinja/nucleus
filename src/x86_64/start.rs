8d7df71da0f266312c6b37e0a1bae4172ba95788
Jason Lu <jason@jasonxlu.com>
2023-01-05 22:58:15 -0600

    REVERT ME: Test allocation

--- src/x86_64/start.rs
+++ src/x86_64/start.rs
@@ -1,21 +1,25 @@
 use super::cpu;
+use super::mm::early;
 use super::serial::SerialLogger;
 use crate::log;
-use limine::LimineMmapRequest;
+use limine::LimineMemmapRequest;
 
-static MMAP_REQUEST: LimineMmapRequest = LimineMmapRequest::new(0);
+static MMAP_REQUEST: LimineMemmapRequest = LimineMemmapRequest::new(0);
 static SERIAL_LOGGER: SerialLogger = SerialLogger::new();
 
 #[no_mangle] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
-    // this function is the entry point, since the linker looks for a function
-    // named `_start` by default
-    MMAP_REQUEST.get_response().get().unwrap();
+    let mmap = MMAP_REQUEST.get_response().get_mut().unwrap();
 
     log::set_log_output(&SERIAL_LOGGER);
     debug!("Begin x86_64 platform init");
 
     cpu::init();
+    let mut early_allocator = early::Allocator::new(mmap.memmap_mut());
+
+    while let Some(addr) = early_allocator.alloc() {
+        info!("Obtained address {:X}", addr);
+    }
 
     crate::kmain();
 }
